4bbea9d49471fe121370fd71aa29ce8a095d891f
Hermela-Fikre <hermelafikre3@gmail.com>
2020-05-13 12:05:26 +0300

    Fix Winner method and valid position on the test file

--- spec/test.rb
+++ spec/test.rb
@@ -2,14 +2,14 @@ require '../lib/game.rb'
 require '../lib/board.rb'
 
 describe Game do
-  subject(:game) {Game.new([1, 2, 3, 4, 5, 6, 7, 8, 9])}
-  
+  subject(:game) { Game.new([1, 2, 3, 4, 5, 6, 7, 8, 9]) }
+
   describe '#initialize' do
-    it 'should initiate a Game class'do
-    expect(game.class).to eq(Game) 
+    it 'should initiate a Game class' do
+      expect(game.class).to eq(Game)
     end
   end
-  
+
   describe '#valid_letter?' do
     it 'checks validity of players\'s choice' do
       expect(Game.valid_letter?('O')).to be true
@@ -38,24 +38,20 @@ describe Game do
   # end
 
   describe '#valid_position?' do
-    arr = Array[1, 2, 3, 4, 5, 6, 7, 8, 9]
     random = rand(1..9)
-    arr.delete_at(random-1)
-    arr.insert(random-1, ' ')
-    expected = arr[random-1]
     it 'checks if selected position is valid' do
-      expect(game.valid_position?(random)).to eql(true)
+      expect(game.valid_position?(random)).to eql(false)
     end
     it 'returns false when a string is used as an argument' do
-      expect(game.valid_position?(String.new)).to be_falsey
+      expect(game.valid_position?('')).to be_falsey
     end
   end
-  
+
   describe '#add_to_table' do
     arr = Array[1, 2, 3, 4, 5, 6, 7, 8, 9]
     random = rand(1..9)
-    arr.delete_at(random-1)
-    expected = arr.insert(random-1, nil)
+    arr.delete_at(random - 1)
+    expected = arr.insert(random - 1, nil)
     it 'should add the player choice into a board' do
       expect(game.add_to_table(random)).to eq(expected)
     end
@@ -63,24 +59,22 @@ describe Game do
 end
 
 describe Board do
-  subject(:board) {Board.new([[1], [2], [3], [4], [5], [6], [7], [8], [9]])}
-  # subject(:board) {Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])}
-  # subject(:board) {Board.new}
-
   describe '#winner?' do
-    letter = 'X'
-    it 'matches players patterns with winning positions' do
-      expect(Board.winner?([0,1,2], letter)).to eq true
+    context 'When there is a winner' do
+      let(:board) { ['O', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' '] }
+      let(:letter) { 'O' }
+      it 'return true' do
+        expect(Board.winner?(board, letter)).to eq true
+      end
     end
   end
 
   describe '#draw?' do
     it 'returns true if board is full' do
-      expect(Board.draw?(['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O'])).to be true
+      expect(Board.draw?(%w[X O X X O X O X O])).to be true
     end
     it 'returns false if board has an empty cell' do
       expect(Board.draw?(['X', ' '])).to be false
     end
-    
   end
 end
